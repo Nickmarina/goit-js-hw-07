63f86c1cb0bc78535130202a75bad3aff8618e29
Marina Permiakova <63551081+Nickmarina@users.noreply.github.com>
2020-12-23 22:55:49 +0200

    Добавляет Task 7

--- js/task_07.js
+++ js/task_07.js
@@ -2,3 +2,21 @@
 // size - control(событие input) и изменяет инлайн - стиль span#text
 // обновляя свойство font - size.В результате при перетаскивании ползунка будет
 // меняться размер текста.
+
+// <input id="font-size-control" type="range" />
+//     <br />
+// <span id="text">Абракадабра!</span>
+
+const inputRef = document.querySelector('#font-size-control');
+const textRef = document.querySelector('#text');
+
+inputRef.addEventListener('input', inputHandler);
+
+function inputHandler(event) {
+  //   //   console.log(event);
+  //   //   console.dir(event.target);
+  //   //   console.log(event.target.value);
+  if (event) {
+    textRef.setAttribute('style', `font-size: ${event.target.value}px;`);
+  }
+}
