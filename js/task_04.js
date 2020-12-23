3ec85b9525c987698849808b46f2523c7bbd0509
Marina Permiakova <63551081+Nickmarina@users.noreply.github.com>
2020-12-23 17:33:14 +0200

    Добавляет Task 4

--- js/task_04.js
+++ js/task_04.js
@@ -2,3 +2,27 @@
 // Создай переменную counterValue в которой будет хранится текущее значение счетчика.
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+/* <div id="counter">
+    <button type="button" data-action="decrement">-1</button>
+    <span id="value">0</span>
+    <button type="button" data-action="increment">+1</button>
+</div> */
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const spanRef = document.querySelector('#value');
+let counterValue = 0;
+
+decrementBtn.addEventListener('click', decrement);
+incrementBtn.addEventListener('click', increment);
+
+function decrement() {
+  counterValue -= 1;
+  return (spanRef.textContent = counterValue);
+}
+
+function increment() {
+  counterValue += 1;
+  return (spanRef.textContent = counterValue);
+}
